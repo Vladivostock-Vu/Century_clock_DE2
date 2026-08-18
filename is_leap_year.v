4731098cd26caef541f7ab80ea39af170f2d324d
Vu Nhat Minh <nhatminhvu2005@gmail.com>
2026-08-19 00:32:03 +0700

    Fix leap year logic and syntax errors in module

--- is_leap_year.v
+++ is_leap_year.v
@@ -11,20 +11,20 @@ assign hundreds = year[11:8];
 assign tens = year[7:4];
 assign units = year[3:0];
 
-// Check if the year is divisible by 4
-//If tens is even, then units must be 0, 4, or 8
+//Check if the year is divisible by 4
+//If tens is even, then units must be divisible by 4
 //If tens is odd, then units must be 2 or 6
-assign div4 = (~tens[0] && (units == 4'd0 || units == 4'd4 || units == 4'd8)) ||
-              ( tens[0] && (units == 4'd2 || units == 4'd6));
+assign div4 = (~tens[0] && (units[1:0] == 2'b00)) ||
+              ( tens[0] && (units[1:0] == 2'b10);
 
 //Check if the year is divisible by 100
-assign div100 = tens == 4'd0 && units == 4'd0;
+assign div100 = {tens, hundreds} == 8'b00000000;
 
 //Check if the year is divisible by 400
 //The year must be divisible by 100 and the year's hundreds must be divisible by 4
 assign div400 = div100 && 
-                (~thousands[0] && (hundreds == 4'd0 || hundreds == 4'd4 || hundreds == 4'd8)) ||
-                ( thousands[0] && (hundreds == 4'd2 || hundreds == 4'd6));
+                (~thousands[0] && (hundreds[1:0] == 2'b00)) ||
+                ( thousands[0] && (hundreds[1:0] == 2'b10);
 
 assign is_leap_year = div4 && (!div100 || div400);
 
