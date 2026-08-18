c3691b43be63e5ab4390f30dc054ecff38d1e9bc
Vu Nhat Minh <nhatminhvu2005@gmail.com>
2026-08-19 00:21:54 +0700

    Add files via upload

---
+++ is_leap_year.v
@@ -0,0 +1,31 @@
+module is_leap_year (
+    input wire [15:0] year, //Year in BCD format
+    output wire is_leap_year,
+);
+
+wire [3:0] thousands, hundreds, tens, units;
+wire div4, div100, div4_hundreds, div400;
+
+assign thousands = year[15:12];
+assign hundreds = year[11:8];
+assign tens = year[7:4];
+assign units = year[3:0];
+
+// Check if the year is divisible by 4
+//If tens is even, then units must be 0, 4, or 8
+//If tens is odd, then units must be 2 or 6
+assign div4 = (~tens[0] && (units == 4'd0 || units == 4'd4 || units == 4'd8)) ||
+              ( tens[0] && (units == 4'd2 || units == 4'd6));
+
+//Check if the year is divisible by 100
+assign div100 = tens == 4'd0 && units == 4'd0;
+
+//Check if the year is divisible by 400
+//The year must be divisible by 100 and the year's hundreds must be divisible by 4
+assign div400 = div100 && 
+                (~thousands[0] && (hundreds == 4'd0 || hundreds == 4'd4 || hundreds == 4'd8)) ||
+                ( thousands[0] && (hundreds == 4'd2 || hundreds == 4'd6));
+
+assign is_leap_year = div4 && (!div100 || div400);
+
+endmodule
